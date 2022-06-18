320de7fbaa01363d1d97a72b773736fed575e897
sina <sinaolfati5@gmail.com>
2022-06-18 16:45:20 +0430

    making the Design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home/>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from "./Home.module.css"
-import icon from "./imgaes/Shape.png"
+import icon from "./imgaes/shape.png"
 
 const Home = () => {
   return (
     <div className={styles.container}>
-        <img src={icon}/>
-      
+        <img src={icon} className={styles.Shape}/>
+        <span className={styles.Shape2}></span>
     </div>
   )
 }
