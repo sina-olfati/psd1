9cbaf1e552c4cd7307fb193c7565e3f1032cce7a
sina <sinaolfati5@gmail.com>
2022-06-18 12:49:15 +0430

    Showing svg (failed)

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Home.module.css"
+import icon from "./imgaes/Shape.png"
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+        <img src={icon}/>
+      
+    </div>
+  )
+}
+
+export default Home
